fe7100d748bb7865f31c07068074ab5c04efdc93
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-03-11 20:12:59 +0800

    Implemented and tested spatial weights matrix visualization

--- server/analysis.py
+++ server/analysis.py
@@ -1,6 +1,8 @@
 import geopandas as gpd
-from spatial_weights import contiguity_weights, knn_weights, distance_weights
 from sqlalchemy import create_engine
+
+from spatial_weights import contiguity_weights, knn_weights, distance_weights
+from visualization import visualize_neighbors
 
 host = "localhost"
 port = "5432"
@@ -23,7 +25,8 @@ gdf = gpd.read_postgis(sql_query, engine, geom_col="geom")
 # print("CRS:", gdf.crs)
 
 # w = distance_weights(gdf)
-# w = knn_weights(gdf, k=3)
-w = contiguity_weights(gdf)
+w = knn_weights(gdf, k=3)
+# w = contiguity_weights(gdf)
 
-print("Neighbors:", w.neighbors)
+# print("Neighbors:", w.neighbors)
+visualize_neighbors(gdf, w)
